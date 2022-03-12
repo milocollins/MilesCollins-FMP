e12250f7d357d4a8912de8f2700da6e1b1b2be0b
MilesDAC <5collm30@solent.ac.uk>
2022-03-12 16:44:01 +0000

    Gate Logic + VFX Added

--- Assets/ConsoleHealth.cs
+++ Assets/ConsoleHealth.cs
@@ -5,4 +5,23 @@ using UnityEngine;
 public class ConsoleHealth : HealthManager
 {
     public GameObject barrier;
+    public GameObject consoleVFX;
+    public override void TakeDamage(Vector3 location, Vector3 direction, float damage, Collider bodyPart = null, GameObject origin = null)
+    {
+        if (!dead)
+        {
+            dead = true;
+            if (barrier != null)
+            {
+                StartCoroutine(CreateVFX(location));
+                Destroy(barrier);
+            }
+        }
+    }
+    private IEnumerator CreateVFX(Vector3 location)
+    {
+        GameObject VFX = Instantiate(consoleVFX, location, Quaternion.Euler(-90f, 0f, 0f));
+        yield return new WaitForSeconds(30f);
+        Destroy(VFX);
+    }
 }
